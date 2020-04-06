91e23a003a5d6c9c2fcef7c791afa0444fce40cc
Guandalfo <Sneakynova@outlook.com>
2020-04-06 15:26:26 +0200

    H02

---
+++ src/h02/opdracht1.java
@@ -0,0 +1,21 @@
+package h02;
+
+
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class opdracht1 extends Applet {
+
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Nova", 50, 60 );
+    }
+
+}
---
+++ src/h02/opdracht2.java
@@ -0,0 +1,21 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class opdracht2 extends Applet {
+
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Nova", 50, 60);
+        g.setColor(Color.red);
+        g.drawString("Bank", 50, 70);
+
+    }
+}
